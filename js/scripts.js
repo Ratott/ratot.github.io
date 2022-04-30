5f98afee70cfd771c74ad26a1e95f7787f02fd40
Ratot <rat0t@hotmail.com>
2022-05-01 01:17:01 +0200

    :)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let display = false;
 let pages = [document.getElementById("page1"),document.getElementById("page2"),document.getElementById("page3"),document.getElementById("page4")];
+let titles = ["Home","Test","Test","About"];
 
 let toggle = button => {
   let element = document.getElementById("list");
@@ -17,6 +18,7 @@ let changePage = (page) => {
   for(let i = 0;i<pages.length;i++){
     if(page === i){
       pages[i].classList.remove("hidden");
+      document.title = "Ratot - " + titles[i];
     }
     else{
       pages[i].classList.add("hidden");
