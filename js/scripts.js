4a890ca8d6e01523fae1a08ced2c694009cb8055
Ratot <rat0t@hotmail.com>
2022-07-22 03:35:36 +0200

    :)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let display = true;
 let pages = [document.getElementById("page1"),document.getElementById("page2"),document.getElementById("page3"),document.getElementById("page4")];
-let titles = ["Home","Ideas","Test","About"];
+let titles = ["Home","Projects","Test","About"];
 
 let toggle = button => {
   let element = document.getElementById("list");
